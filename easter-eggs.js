3cd9075c9919dcf4b082e7b3e101ac994732f3ab
Kevin Quispe <kevincarlosqa@gmail.com>
2023-05-18 20:47:47 -0500

    add: Switch Options

--- easter-eggs.js
+++ easter-eggs.js
@@ -1,14 +1,42 @@
 function TicTacToe() {
+  console.log("tic tac");
 }
 
 function Loader() {
+  console.log("loader");
 }
 
 function Clock() {
+  console.log("clock");
 }
 
-function numberFormatter() {
+function NumberFormatter(...rest) {
+  console.log("number");
+  this.rest = rest
+  console.log(rest);
+  this.snippet = function (number){
+    console.log(number)
+  }
+
 }
 
-function EasterEgg() {
+function EasterEgg(program,...rest) {
+  let instance
+  switch(program){
+    case "clock":
+      instance = new Clock()
+      return instance
+      // break;
+    case "tic tac toe":
+      instance = new TicTacToe()
+      return instance
+      // break;
+    case "number formatter":
+      instance = new NumberFormatter(...rest)
+      return instance
+      // break;
+  }
 }
+
+
+// game = new EasterEgg("number formatter",1000,["g","Kg"])
